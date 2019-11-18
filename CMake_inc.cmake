1a9a8e67b47ffe4a8a2c4d1be32940d1e37b4cf6
Gleb Novodran <novodran@gmail.com>
2019-11-18 12:11:30 +0200

    gcc compilation params change

--- CMake_inc.cmake
+++ CMake_inc.cmake
@@ -1,7 +1,7 @@
 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-set(GCC_RELEASE_COMPILE_FLAGS "-O3 -mavx2 -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
+set(GCC_RELEASE_COMPILE_FLAGS "-O3 -mavx -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
 
-set(GCC_DEBUG_COMPILE_FLAGS " -ggdb -mavx2 -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
+set(GCC_DEBUG_COMPILE_FLAGS " -ggdb -mavx -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
 
 set (CMAKE_CXX_FLAGS_RELEASE ${GCC_RELEASE_COMPILE_FLAGS} )
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE  "${CMAKE_EXE_LINKER_FLAGS} ${GCC_RELEASE_COMPILE_FLAGS}" )
